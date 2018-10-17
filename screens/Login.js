36c1ca82be22bb6ae9f00b847c290b51da7d9dec
Exception <40845910+Exceptionsk@users.noreply.github.com>
2018-10-17 19:42:21 +0630

    splash screen, dynamic home component, navi

--- screens/Login.js
+++ screens/Login.js
@@ -1,16 +1,37 @@
 import React, {Component} from "react";
-import {View, Text, Button} from "react-native";
-
-class Login extends Component {
+import {View,Image} from "react-native";
+import {Container, Button, Left, Right, Icon, Text} from 'native-base';
+import { Col, Row, Grid } from 'react-native-easy-grid';
+import { Ionicons } from '@expo/vector-icons';
+export default class Login extends Component {
   static navigationOptions = {
     header:null
   }
   render(){
     return(
-      <View>
-        <Button title="Login" onPress={() => this.props.navigation.replace('CustHome')}/>
-      </View>
+      <Container style={{backgroundColor:'white'}}>
+      <Grid>
+        <Row>
+        </Row>
+        <Row>
+          <Col>
+            <View style={{alignItems: 'center'}}>
+              <Image source={require('../assets/splash.png')} style={{height:160, width:160}} />
+            </View>
+            <View style={{alignItems: 'center'}}>
+              <Button iconLeft full primary textStyle={{color:'white'}} style={{alignSelf:'center'}} onPress={() => this.props.navigation.replace('CustHome')}>
+                <Icon name='logo-facebook' />
+                <Text> Login with Facebook </Text>
+              </Button>
+            </View>
+          </Col>
+        </Row>
+        <Row>
+        </Row>
+      </Grid>
+
+      </Container>
+
     )
   }
 };
-export default Login;
