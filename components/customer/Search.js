b3818427d18ad44335702db6eae4cd5594112098
Exception <minthukhant.it@gmail.com>
2018-11-30 10:57:08 +0630

    restaurant search

--- components/customer/Search.js
+++ components/customer/Search.js
@@ -4,17 +4,7 @@ import { MaterialCommunityIcons,Ionicons } from '@expo/vector-icons';
 
 export default class Home extends Component{
 
-  constructor()
-  {
-    super();
-    this.items = [
-      {name:'KFC', favouritestatus:'yes', img:'https://myanimelist.cdn-dena.com/images/anime/1536/93863l.jpg'},
-      {name:'Sar Mal',favouritestatus:'no', img:'https://myanimelist.cdn-dena.com/images/anime/1536/93863l.jpg'},
-      {name:'Gone Sin',favouritestatus:'yes', img:'https://myanimelist.cdn-dena.com/images/anime/1536/93863l.jpg'},
-      {name:'YKKO',favouritestatus:'yes', img:'https://myanimelist.cdn-dena.com/images/anime/1536/93863l.jpg'},
-      {name:'Golden Pot',favouritestatus:'no', img:'https://myanimelist.cdn-dena.com/images/anime/1536/93863l.jpg'},
-    ];
-  }
+
   FavouriteButtonStatus(favouritestatus){
     if(favouritestatus=='yes'){
       return <Button transparent warning style={{height:'55%',shadowColor: '#000000',shadowOffset: {width: 1,height: 1},shadowRadius: 1,shadowOpacity: 0.5}}><Badge warning style={{height:'100%', backgroundColor:'white', borderWidth:1, borderColor:'#fe680d'}}><MaterialCommunityIcons name="tag-heart" size={35} color="red" /></Badge></Button>
@@ -23,13 +13,30 @@ export default class Home extends Component{
       return <Button transparent warning style={{height:'55%',shadowColor: '#000000',shadowOffset: {width: 1,height: 1},shadowRadius: 1,shadowOpacity: 0.5}}><Badge style={{height:'100%', backgroundColor:'white', borderWidth:1, borderColor:'#fe680d'}}><MaterialCommunityIcons name="tag-heart" size={35} color="#6c7a87"/></Badge></Button>
     }
   }
+
+  handleSearch(value){
+    return fetch(global.HostURL + 'api/restaurant/' + value)
+    .then((response) => response.json())
+    .then((responseJson) => {
+      console.log(responseJson);
+      this.setState({
+         Users: responseJson,
+       }, function(){
+
+       });
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+  }
+
   render(){
     return(
       <Container>
         <Header searchBar rounded style = {{height: 70,backgroundColor: '#a3080c', color: 'orange', paddingBottom: 10, paddingTop: 20}}>
             <Item style = {{backgroundColor: 'white' ,borderColor: 'orange', borderTopWidth: 1, borderRightWidth: 1, borderLeftWidth: 1}}>
               <Icon name="ios-search" />
-              <Input placeholder="Search" />
+              <Input placeholder="Search"  onSubmitEditing={(value)=>this.handleSearch.bind(value)} />
             </Item>
             {/* <Button transparent onPress={()=>this.props.navigation.goBack()} >
               <Icon style = {{color: 'white', paddingLeft:20}} name='arrow-back' />
